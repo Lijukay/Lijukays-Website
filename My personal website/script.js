0dc926f8280ee73287d553865a4cc7b164682b0b
Lijukay <lico.keins@gmail.com>
2023-03-29 16:14:46 +0200

    det

--- My personal website/script.js
+++ My personal website/script.js
@@ -4,6 +4,7 @@ const body = document.querySelector("body"),
   searchBtn = body.querySelector(".search-box"),
   modeSwitch = body.querySelector(".toggle-switch"),
   modeText = body.querySelector(".mode-text");
+const home = body.querySelector(".home");
 
 toggle.addEventListener("click", () => {
   sidebar.classList.toggle("close");
@@ -20,5 +21,16 @@ modeSwitch.addEventListener("click", () => {
     modeText.innerText = "Light mode";
   } else {
     modeText.innerText = "Dark mode";
+  }
+});
+const toggleButton = document.querySelector(".toggle-button");
+
+body.addEventListener("click", (event) => {
+  const target = event.target;
+  const isClickInsideSidebar = sidebar.contains(target);
+  const isClickInsideToggleButton = toggleButton.contains(target);
+
+  if (!isClickInsideSidebar && !isClickInsideToggleButton) {
+    home.classList.remove("close");
   }
 });
